c878488660568dbd678f515ecc420b58ff0059c4
GovindSahu123 <govind.sahu@valuecreatives.com>
2025-06-08 14:32:22 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ dotenv.config({
 
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.error("MONGO db connection failed!!", err);
+    });
