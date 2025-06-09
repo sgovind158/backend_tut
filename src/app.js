86fd59c98058c895b4b195778c0ddbf709f76ed2
GovindSahu123 <govind.sahu@valuecreatives.com>
2025-06-09 10:39:56 +0530

    add register controller

--- src/app.js
+++ src/app.js
@@ -23,6 +23,13 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//router imports
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 
 
 
