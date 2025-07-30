bf76eb783151f26945731f952e14b83fc3a6ecba
sgovind158 <govindsahu2842@gmail.com>
2025-07-30 23:30:32 +0530

    delete all

--- src/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js';
-
-const router = Router();
-router.post('/register', registerUser);
-
-export default router;
