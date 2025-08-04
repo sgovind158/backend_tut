5ed37254a7d1147e2f3de752ddf87815a96d7f9a
sgovind158 <govindsahu2842@gmail.com>
2025-08-05 00:32:47 +0530

    delete all file

--- chepter7-npm and tools/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log("Server is running");   
-});
-
-const PORT = 3000;
-server.listen(PORT,()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
