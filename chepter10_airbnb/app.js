d46d5a83cc979ae64182f5a3ec7bf8d827aa001b
sgovind158 <govindsahu2842@gmail.com>
2025-08-11 12:36:09 +0530

    chapter 10 done

---
+++ chepter10_airbnb/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+const rootDir = require('./utils/pathUtil');
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host",hostRouter);
+
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
